6d89a334ec919fd95516a44b1eacf8d958afbb0f
Adepoju Lateef <badepojulateef@gmail.com>
2023-06-19 10:03:19 +0100

    updated task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,7 +6,7 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 
 -- Grant all privileges on hbnb_dev_db to hbnb_dev
-GRANT ALL PRIVILEGES ON hdnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
 
 -- Grant SELECT privilege on performance_schema to hbnb_dev
