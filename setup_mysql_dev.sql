2bb5bbada9a5101ced457aeca27d90448b5101e5
Adepoju Lateef <badepojulateef@gmail.com>
2023-06-19 09:59:09 +0100

    updated task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,6 +4,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- Create the hbnb_dev_db user if it doesn't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
+
 -- Grant all privileges on hbnb_dev_db to hbnb_dev
 GRANT ALL PRIVILEGES ON hdnb_dev_db.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
