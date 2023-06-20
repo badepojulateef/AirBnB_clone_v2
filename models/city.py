41b6a156641b48a81c96076a81a0723e4f924fd6
Adepoju Lateef <badepojulateef@gmail.com>
2023-06-20 18:09:32 +0100

    reverted

--- models/city.py
+++ models/city.py
@@ -2,36 +2,11 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-<<<<<<< HEAD
-from sqlalchemy.orm import relationship
-from sqlalchemy import storage_type
-=======
->>>>>>> parent of be54c57... added task 8
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-<<<<<<< HEAD
-    if storage_type == 'db':
-
-        __tablename__ = 'cities'
-
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-
-        places = relationship(
-            "Place", backref="cities", cascade="all, delete-orphan"
-        )
-    else:
-        name = ""
-        state_id = ""
-        
-        def __init__(self, *args, **kwargs):
-            """ Initializes city """
-            super().__init__(*args, **kwargs)
-=======
     __tablename__= 'cities'
 
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
->>>>>>> parent of be54c57... added task 8
