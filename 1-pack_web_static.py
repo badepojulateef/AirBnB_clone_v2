6784f71b1959949ba11f8a2c1b94a2ad69d6887d
Adepoju Lateef <badepojulateef@gmail.com>
2023-07-06 18:36:56 +0100

    added task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+A Fabric script that generates a .tgz archive from the contents
+"""
+import os
+from fabric.api import local
+from datetime import datetime
+
+env.user = 'ubuntu'
+env.hosts = ['107.22.142.218']
+
+
+def do_pack():
+    """
+    generates a .tgz archive from the contents
+    """
+    try:
+        local("mkdir -p versions")
+        now = datetime.now()
+        timestamp = now.strftime("%Y%m%d%H%M%S")
+        archive_name = "web_static_{}.tgz".format(timestamp)
+        archive_path = "versions/{}".format(archive_name)
+        local("tar -czvf {} web_static".format(archive_path))
+        if os.path.exists(archive_path):
+            return archive_path
+        else:
+            return None
+    except Exception as e:
+        return None
