b43a0b9ab00ce12628f60c63ccb80ed4e3b207b3
Adepoju Lateef <badepojulateef@gmail.com>
2023-07-06 18:36:41 +0100

    added task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# This script sets up web servers for the deployment of web_static
+
+# Install Nginx if it's not already installed
+if ! command -v nginx > /dev/null; then
+	sudo apt-get update
+	sudo install nginx
+	sudo ufw allow 'Nginx HTTP'
+fi
+
+# Create necessary directories
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+
+# Create a fake HTML file for testing
+echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+
+# Create symbolic link
+sudo rm -rf /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Update Nginx configuration
+sudo chown 755 -R ubuntu:ubuntu /data/
+
+# Update Nginx configuration
+sudo sed -i '44i\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
+
+# check the syntax of the conf
+sudo nginx -t
+
+# Restart Nginx
+sudo service nginx restart
